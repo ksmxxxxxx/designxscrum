aee8541a948424d0d30a774514d48d1b1dc19398
ksmxxxxxx <kankichi69@gmail.com>
2021-06-26 11:19:46 +0900

    :construction: Configure 11ty

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+const pluginSass = require('eleventy-plugin-sass')
+const pug = require('pug')
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('_sources/images')
+  eleventyConfig.setLibrary('pug', pug)
+  eleventyConfig.addPlugin(pluginSass, {
+    autoprefixer: true,
+    sourcemap: false,
+    watch: ['_sources/stylesheets/*.{sass, scss}'],
+    outputDir: '_site/stylesheets'
+  })
+
+  return {
+    dir: {
+      input: '_sources',
+      data: '_data'
+    },
+    markdownTemplateEngine: 'md'
+  }
+}
